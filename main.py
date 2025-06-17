156f97ee8bc014007f947a3107a7b99440f0f387
v1k3kk <mitchenkoviktor111@gmail.com>
2025-06-17 21:19:41 +0200

    Api подключено. Бот запускается.

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-213123
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = "8071043602:AAHDmlntOwHbEs5tCcJDDQeb6i0B-XecXDs"
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hello!")
+def run_bot():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot started")
+    app.run_polling()
+if __name__ == "__main__":
+    run_bot()
